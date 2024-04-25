ca8cc7e3a1e3c4760d513b90f4100bd13823bf3c
sahilsinghavisoft <sahil.singh@avisoft.io>
2024-04-25 17:16:53 +0530

    fourth commit midrange

--- weeklyassignment8/ml_deployment/utils/filesloader.py
+++ weeklyassignment8/ml_deployment/utils/filesloader.py
@@ -1,6 +1,6 @@
 import pickle
 
-with open("ml_deployment/Datafiles/model_lr.pkl", "rb") as f:
+with open("ml_deployment/Datafiles/modelgridsearch_svc.pkl", "rb") as f:
     model = pickle.load(f)
 with open("ml_deployment/Datafiles/scaler.pkl", "rb") as f:
     scaler = pickle.load(f)
